f6887b461ed5cf3920cd6e483474611df19bc90b
Alen Livojević <alenliv12@gmail.com>
2023-01-11 20:35:32 +0100

    modality and difficulty added

--- app/src/main/java/com/example/hci/activities/SettingsActivity.java
+++ app/src/main/java/com/example/hci/activities/SettingsActivity.java
@@ -8,9 +8,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.hci.R;
 
@@ -25,13 +27,23 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
 
-        username =findViewById(R.id.username);
+        username = findViewById(R.id.username);
         username.setText(sharedPref.getString("username", ""));
         back = findViewById(R.id.back_button);
 
         training = findViewById(R.id.training);
         if(sharedPref.getString("training", "").equals("true")) training.setChecked(true);
         else training.setChecked(false);
+
+        Spinner modality = findViewById(R.id.modality);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.modality, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        modality.setAdapter(adapter);
+
+        Spinner difficulty = findViewById(R.id.difficulty);
+        ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this, R.array.difficulty, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        difficulty.setAdapter(adapter2);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,10 +57,15 @@ public class SettingsActivity extends AppCompatActivity {
                 } else{
                     editor.putString("training", "false");
                 }
+                editor.putString("difficulty", difficulty.getSelectedItem().toString());
+                editor.putString("modality", modality.getSelectedItem().toString());
                 editor.apply();
-                //Log.i("USERNAME: ", sharedPref.getString("username", ""));
-               // Log.i("TRAINING: ", sharedPref.getString("training", ""));
-
+                /*
+                Log.i("USERNAME: ", sharedPref.getString("username", ""));
+                Log.i("TRAINING: ", sharedPref.getString("training", ""));
+                Log.i("DIFFICULTY: ", sharedPref.getString("difficulty", ""));
+                Log.i("MODALITY: ", sharedPref.getString("modality", ""));
+                */
                 Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
                 startActivity(intent);
             }
